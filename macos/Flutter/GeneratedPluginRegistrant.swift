724d6907b6334c5e685f38bf8a2ade8dbd97e98c
Kevin Kamto <kamtokevin@gmail.com>
2026-02-18 10:19:41 -0500

    feat(deps): add in_app_purchase and shared_preferences
    Add in_app_purchase ^3.2.0 for iOS StoreKit IAP flow and
    shared_preferences ^2.3.3 for persisting purchase/trial state
    locally. Update lock file and regenerate plugin registrant.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,12 @@ import Foundation
 
 import firebase_auth
 import firebase_core
+import in_app_purchase_storekit
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
